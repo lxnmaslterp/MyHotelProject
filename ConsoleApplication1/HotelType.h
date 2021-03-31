7455553a616b1489aea042c770470d25c7c3366c
lxnmaslterp <lxnmaslterp@gmail.com>
2021-03-31 23:10:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/HotelType.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class HotelType
+{
+public:
+
+private:
+	std::string address;
+	int NumberOfRooms;
+	int NumberOfRoomsAvailable;
+};
+
+
+class RoomType
+{
+public:
+
+};
